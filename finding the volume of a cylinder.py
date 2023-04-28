85f44a1675ac33aaf8934e9432fd4eae046292b3
ezeamii <131669112+ezeamii@users.noreply.github.com>
2023-04-28 13:44:58 +0100

    Add files via upload

---
+++ finding the volume of a cylinder.py
@@ -0,0 +1,8 @@
+# PROGRAM THAT FINDS THE VOLUME OF A CYLINDER
+radius = int(input("Input the radius of the cylinder: "))
+height = int(input("Input the height of the cylinder: "))
+pi = 3.14
+
+volume = (pi * (radius * radius) * height)
+
+print("The volume of the cylinder is", volume, "cm^3")
